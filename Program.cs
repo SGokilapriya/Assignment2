1ab48024fd4da4c2af530952913e7f0a4a45737b
SGokilapriya <36201974+SGokilapriya@users.noreply.github.com>
2020-08-17 09:00:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tictactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Displayboard();
+        }
+        public void Displayboard()
+        {
+            int board = 6;
+            Console.WriteLine(board);
+        }
+    }
+}
